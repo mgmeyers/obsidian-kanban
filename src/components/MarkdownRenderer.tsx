80e9a0ea1a5913a24ab975da1a8ae3526fbf58b6
PJ <pje@telecommunity.com>
2021-06-12 19:49:03 -0400

    Minimize DOM updates
    Technically not part of parsing, but since the parser is figuring
    out the DOM nodes anyway, might as well use them properly in
    the rendering.

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Mark from "mark.js";
 import { ObsidianContext } from "./context";
+import { findDOMNode } from "react-dom";
 
 interface MarkdownRendererProps {
   className?: string;
@@ -9,26 +10,70 @@ interface MarkdownRendererProps {
   searchQuery?: string;
 }
 
-export const MarkdownRenderer = React.memo(
-  ({ className, dom, markdownString, searchQuery }: MarkdownRendererProps) => {
-    const { view } = React.useContext(ObsidianContext);
-    const markdownContent = React.useMemo(() => {
-      let tempEl: HTMLDivElement = dom || view.renderMarkdown(markdownString);
-      if (searchQuery) {
-        tempEl = tempEl.cloneNode(true) as HTMLDivElement;
-        new Mark(tempEl as HTMLDivElement).mark(searchQuery);
-      }
+interface MarkdownRendererState {
+  parsedEl?: HTMLDivElement
+  marker?: Mark;
+}
 
-      return {
-        innerHTML: { __html: tempEl.innerHTML.toString() },
-      };
-    }, [dom, markdownString, view, searchQuery]);
+export class MarkdownRenderer extends React.Component<MarkdownRendererProps, MarkdownRendererState> {
+  static contextType = ObsidianContext;
+  props: MarkdownRendererProps;
 
+  constructor(props: MarkdownRendererProps) {
+    super(props);
+    // Always maintain state
+    this.state = this.refreshState(props);
+  }
+
+  parse(props: MarkdownRendererProps) {
+    return props.dom.cloneNode(true) || this.context.view.renderMarkdown(this.props.markdownString);
+  }
+
+  refreshState(props: MarkdownRendererProps, state: MarkdownRendererState={}) {
+    let {parsedEl, marker} = state;
+    if (!parsedEl) parsedEl = this.parse(props);
+    if (!marker) {
+      marker = new Mark(parsedEl)
+    } else {
+      marker.unmark();
+    }
+    if (props.searchQuery) marker.mark(props.searchQuery);
+    return (parsedEl !== state.parsedEl || marker !== state.marker ) ? {parsedEl, marker} : state;
+  }
+
+  shouldComponentUpdate(nextProps: MarkdownRendererProps, nextState: MarkdownRendererState) {
+    // Ignore changes to state, as we only set state from componentDidUpdate
+    const res= (
+      nextProps.dom !== this.props.dom ||
+      nextProps.className !== this.props.className ||
+      nextProps.markdownString !== this.props.markdownString ||
+      nextProps.searchQuery !== this.props.searchQuery
+    );
+    return res
+  }
+
+  componentDidMount() {
+    findDOMNode(this).appendChild(this.state.parsedEl)
+  }
+
+  componentDidUpdate(prevProps: MarkdownRendererProps) {
+    let {parsedEl, marker} = this.state
+    if (this.props.dom !== prevProps.dom || this.props.markdownString !== prevProps.markdownString) {
+      const newState = {parsedEl, marker} = this.refreshState(this.props, {});
+      this.setState(newState)
+    } else if (this.props.searchQuery !== prevProps.searchQuery) {
+      marker.unmark();
+      if (this.props.searchQuery) marker.mark(this.props.searchQuery)
+    }
+    const me = findDOMNode(this), newDOM = parsedEl;
+    if (me.firstChild) me.replaceChild(newDOM, me.firstChild); else me.appendChild(newDOM);
+  }
+
+  render() {
     return (
       <div
-        className={`markdown-preview-view ${className || ""}`}
-        dangerouslySetInnerHTML={markdownContent.innerHTML}
+        className={`markdown-preview-view ${this.props.className || ""}`}
       />
     );
   }
-);
+}
