2f32a1fd86c726bfb7fd42eebd103aa6b652a313
M. L. Giannotta <sunyatasattva@gmail.com>
2024-05-01 02:10:05 +0200

    Add support for custom labels for inline metadata
    The users have the option to provide custom labels associated to certain
    metadata keys. If provided, these options will be respected.

--- src/components/Item/InlineMetadata.tsx
+++ src/components/Item/InlineMetadata.tsx
@@ -2,6 +2,7 @@ import classcat from 'classcat';
 import { useContext } from 'preact/compat';
 import { StateManager } from 'src/StateManager';
 import {
+  InlineField,
   getDataviewPlugin,
   lableToIcon,
   lableToName,
@@ -10,7 +11,7 @@ import {
 
 import { SearchContext } from '../context';
 import { c } from '../helpers';
-import { Item } from '../types';
+import { DataKey, Item, PageData } from '../types';
 import { MetadataValue } from './MetadataTable';
 
 interface InlineMetadataProps {
@@ -18,8 +19,26 @@ interface InlineMetadataProps {
   stateManager: StateManager;
 }
 
+function parseMetadataWithOptions(data: InlineField, metadataKeys: DataKey[]): PageData {
+  const options = metadataKeys.find((opts) => opts.metadataKey === data.key);
+
+  return options
+    ? {
+        ...options,
+        value: data.value,
+      }
+    : {
+        containsMarkdown: false,
+        label: data.key,
+        metadataKey: data.key,
+        shouldHideLabel: false,
+        value: data.value,
+      };
+}
+
 export function InlineMetadata({ item, stateManager }: InlineMetadataProps) {
   const search = useContext(SearchContext);
+  const metaKeys = stateManager.getSetting('metadata-keys');
   const moveMetadata = stateManager.useSetting('move-inline-metadata');
   const moveTaskMetadata = stateManager.useSetting('move-task-metadata');
   const { inlineMetadata } = item.data.metadata;
@@ -30,15 +49,16 @@ export function InlineMetadata({ item, stateManager }: InlineMetadataProps) {
   return (
     <span className={c('item-task-metadata')}>
       {inlineMetadata.map((m, i) => {
-        const isTaskMetadata = taskFields.has(m.key);
+        const { metadataKey: key, label: metaLabel, value } = parseMetadataWithOptions(m, metaKeys);
+        const isTaskMetadata = taskFields.has(key);
         if (!moveTaskMetadata && isTaskMetadata) return null;
         if (!moveMetadata && !isTaskMetadata) return null;
 
         const isEmoji = m.wrapping === 'emoji-shorthand';
-        const val = dataview?.api?.parse(m.value) ?? m.value;
-        const isEmojiPriority = isEmoji && m.key === 'priority';
+        const val = dataview?.api?.parse(value) ?? value;
+        const isEmojiPriority = isEmoji && key === 'priority';
         const isDate = !!val?.ts;
-        let label = isEmoji ? lableToIcon(m.key, m.value) : lableToName(m.key);
+        let label = isEmoji ? lableToIcon(metaLabel, value) : lableToName(metaLabel);
 
         if (!isEmoji) label += ': ';
 
@@ -46,7 +66,7 @@ export function InlineMetadata({ item, stateManager }: InlineMetadataProps) {
           <span
             className={classcat([
               c('item-task-inline-metadata-item'),
-              m.key.replace(/[^a-z0-9]/g, '-'),
+              key.replace(/[^a-z0-9]/g, '-'),
               {
                 'is-task-metadata': isTaskMetadata,
                 'is-emoji': isEmoji,
@@ -64,7 +84,7 @@ export function InlineMetadata({ item, stateManager }: InlineMetadataProps) {
                   data={{
                     value: val,
                     label: label,
-                    metadataKey: m.key,
+                    metadataKey: key,
                     shouldHideLabel: false,
                     containsMarkdown: false,
                   }}
